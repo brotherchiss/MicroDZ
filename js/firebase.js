58d413e2cd486f3dfe0a3eeeaef368a65729f391
brotherchiss <wahyu.ikhsannudin@gmail.com>
2026-02-08 10:02:35 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -6,9 +6,13 @@ import { getFirestore } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "ISI_DARI_FIREBASE",
-  authDomain: "ISI_DARI_FIREBASE",
-  projectId: "ISI_DARI_FIREBASE",
+  apiKey: "AIzaSyDX6AEeU53BxG1Rwcdrwmm3vzSmM6iTT6E",
+  authDomain: "client-login-app-6bdcd.firebaseapp.com",
+  projectId: "client-login-app-6bdcd",
+  storageBucket: "client-login-app-6bdcd.firebasestorage.app",
+  messagingSenderId: "257593926119",
+  appId: "1:257593926119:web:423310b90d1149e0785d42",
+  measurementId: "G-1PZFSQ47BG"
 };
 
 const app = initializeApp(firebaseConfig);
