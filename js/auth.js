2ed89ffd34d4c3a4bea013b57e9c99154f0f7b09
brotherchiss <wahyu.ikhsannudin@gmail.com>
2026-02-08 09:45:43 +0700

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,16 @@
+import { auth } from "./firebase.js";
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.login = async function () {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+    alert("Login berhasil");
+  } catch (e) {
+    alert(e.message);
+  }
+}
